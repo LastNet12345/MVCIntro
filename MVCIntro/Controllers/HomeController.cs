86d8b458ef5fdbaa6828f5ab0fdc55159e3513bf
David Nokto <david.nokto@gmail.com>
2022-12-15 14:01:45 +0100

    Add project files.

---
+++ MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntro.Models;
+using System.Diagnostics;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //Console.WriteLine("Någon klickade på Privacy-länken!");
+            //return View("Index");
+            Hej();
+            //return View(nameof(Index));
+            //return RedirectToAction(nameof(Index));
+
+            return View();
+        }
+
+        public void Hej()
+        {
+            Console.WriteLine("Hej från Home-controller!");
+        }
+
+
+    }
+}
